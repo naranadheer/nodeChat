7cbce7da7ba8e52e586546a9c5ddf3ce4f66eaea
Your Name <you@example.com>
2018-09-11 10:11:30 +0530

    add

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path=require('path')
 const express=require('express')
 var dirPath=path.join(__dirname+'/../public')
 
-var port =process.env.Port || 3000;
+var port =process.env.Port;
 
 
 var app=express();
